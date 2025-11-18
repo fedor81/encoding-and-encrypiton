280548ae4858a4bd1b15fb1c49940ef5649e69a6
Laroxyss <lomonosov_18@mail.ru>
2025-11-18 12:37:02 +0500

    feat(barcode): implement cli execute for Barcode

--- qr_code_generator/src/barcode.rs
+++ qr_code_generator/src/barcode.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use anyhow::Result;
 
 mod characters;
@@ -5,6 +7,7 @@ mod code_set;
 mod unit;
 
 use characters::{CHARS, CODE_LEN, Encoding};
+use image::{ImageBuffer, Luma};
 use unit::Unit;
 
 pub use code_set::CodeSet;
@@ -192,6 +195,24 @@ impl<'a> Code128<'a> {
     }
 }
 
+pub fn draw_barcode<P: AsRef<Path>>(code: &[u8], path: P) -> Result<()> {
+    let width = code.len() as u32;
+    let height = (width / 3) as u32;
+
+    let mut img = ImageBuffer::<Luma<u8>, Vec<u8>>::new(width, height);
+
+    for i in 0..code.len() {
+        // 0 - black, 1 - white
+        let color = if code[i] == 0 { 255 } else { 0 };
+        for y in 0..height {
+            img.put_pixel(i as u32, y, Luma([color]));
+        }
+    }
+
+    img.save(path)?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use pretty_assertions::assert_eq;
@@ -339,5 +360,24 @@ mod tests {
             actual_units: {actual_units:?}\n\
             expect_units: {expected_units:?}",
         );
+    }
+
+    #[test]
+    fn test_draw_works() {
+        let code = vec![1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1];
+        let path = "test_draw_works.png";
+        assert!(draw_barcode(&code, path).is_ok());
+        assert!(Path::new(path).exists());
+        std::fs::remove_file(path).unwrap();
+    }
+
+    #[test]
+    fn test_draw_barcode_works() {
+        let code =
+            Code128::encode_with_codeset("Hello World", CodeSet::B).expect("Code128 doesn't work");
+        let path = "test_draw_barcode_works.png";
+        assert!(draw_barcode(&code, path).is_ok());
+        assert!(Path::new(path).exists());
+        std::fs::remove_file(path).unwrap();
     }
 }
