dfe2141f1d1c2340615407655966797077de3666
Laroxyss <lomonosov_18@mail.ru>
2025-11-18 12:37:02 +0500

    feat(barcode): implement cli execute for Barcode

--- qr_code_generator/src/bin/generator.rs
+++ qr_code_generator/src/bin/generator.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use anyhow::Result;
 use clap::{Args, Parser, Subcommand, ValueEnum};
@@ -7,7 +7,7 @@ use qr_code_generator::barcode::{Code128, CodeSet};
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-    Ok(())
+    cli.command.execute()
 }
 
 #[derive(Parser)]
@@ -24,7 +24,7 @@ enum Command {
     /// Команда для построения штрих кода Code 128.
     Barcode {
         #[command(flatten)]
-        input_args: AppArgs,
+        args: AppArgs,
 
         /// Стартовый используемый набор символов. Набор можно переключать внутри input используя специальные символы: À Ɓ Ć
         #[arg(short, long, default_value_t)]
@@ -42,7 +42,7 @@ struct AppArgs {
     path: PathBuf,
 }
 
-#[derive(ValueEnum, Clone, Default, Debug)]
+#[derive(ValueEnum, Copy, Clone, Default, Debug)]
 #[value(rename_all = "UPPERCASE")]
 enum BarcodeSet {
     /// Символы с кодами 0–95: A-Z, 0-9, специальные символы и FNC 1-4
@@ -56,6 +56,16 @@ enum BarcodeSet {
     C,
 }
 
+impl From<BarcodeSet> for CodeSet {
+    fn from(value: BarcodeSet) -> Self {
+        match value {
+            BarcodeSet::A => CodeSet::A,
+            BarcodeSet::B => CodeSet::B,
+            BarcodeSet::C => CodeSet::C,
+        }
+    }
+}
+
 impl std::fmt::Display for BarcodeSet {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -63,5 +73,20 @@ impl std::fmt::Display for BarcodeSet {
             BarcodeSet::B => write!(f, "B"),
             BarcodeSet::C => write!(f, "C"),
         }
+    }
+}
+
+impl Command {
+    pub fn execute(&self) -> Result<()> {
+        match self {
+            Self::Barcode { args, codeset } => {
+                Self::execute_barcode(&args.input, *codeset, &args.path)
+            }
+        }
+    }
+
+    fn execute_barcode<P: AsRef<Path>>(data: &str, barcodeset: BarcodeSet, path: P) -> Result<()> {
+        let code = Code128::encode_with_codeset(&data, CodeSet::from(barcodeset))?;
+        qr_code_generator::barcode::draw_barcode(&code, path)
     }
 }
